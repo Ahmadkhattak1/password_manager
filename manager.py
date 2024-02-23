11f53bac79b78e52acdee864e609abbfdbc6a376
Ahmadkhattak1 <46068424+Ahmadkhattak1@users.noreply.github.com>
2024-02-24 01:18:01 +0500

    Password view works, password save works. (No encryption yet)

--- manager.py
+++ manager.py
@@ -8,14 +8,18 @@ def main():
     
     #checking if the user exists
     if os.path.exists(username_path):
-        print(f"Welcome back {username}")
+        print(f"Welcome back {username.capitalize()}!")
+        
+        with open(username_path, "r") as file:
+            key = file.readline().encode()
+
     
     else:
         print("Creating user...")
         key = Fernet.generate_key()
         
         with open(username_path, "w") as file:
-            file.write(f"{key}")
+            file.write(f"{key.decode()}")
             print(f"User {username} has been created")
             
     fernet = Fernet(key)
@@ -23,15 +27,13 @@ def main():
     while True:
         print("Do you want to 'Save', 'View' or 'Exit'", end="")
         choice = input().lower()
-        
         if choice == "save":
             save(fernet, username)
-            break
         elif choice == "view":
             view(fernet, username)
-            break
         elif choice == "exit":
             sys.exit()
+        
             
             
 def save(fernet, username):
@@ -41,17 +43,24 @@ def save(fernet, username):
     account_username = input("Enter your username: ")
     account_password = input("Enter your password: ")
     
-    with open(user_path, "w") as file:
+    
+    with open(user_path, "a") as file:
         file.write(f"{platform_name} | {account_username}, {account_password}\n")
         print(f"Data has been saved for {platform_name}")
     
     
 def view(fernet, username):
     user_path = f"files/{username}_data.txt"
-    
     with open(user_path, "r") as file:
-        
-
-    
+        for line in file:
+            data = line
+            for i in range(0, len(data)):
+                if data[i] == "|":
+                    platform = data[:i-1]
+                    account = data[i+1:]
+                    username, password = account.split(",")
+                    print(f"Platform: {platform}")
+                    print(f"Username: {username}")
+                    print(f"Password: {password}")
 if __name__ == "__main__":
     main()
